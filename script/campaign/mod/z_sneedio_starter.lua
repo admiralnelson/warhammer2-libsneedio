2239d9b644fd2e3db36cdef6a11b758f0446f30a
admiralnelson <admiralofinternetmeme@outlook.com>
2021-10-05 18:08:39 +0800

    restructure script fix loading issue (make sure client script like knights of round table can load their audio first before init)

--- script/campaign/mod/z_sneedio_starter.lua
+++ script/campaign/mod/z_sneedio_starter.lua
@@ -3,14 +3,7 @@ local print = print2 or out;
 local core = core;
 
 CM:add_first_tick_callback(function(ctx)
-	if(core == nil) then
-		print("FUCK off CORE was null");
-	else
-		print("core object existed")
-	end
-
 	local sneedio = sneedio or require("sneedio");
-
 	if CM ~= nil then
 		sneedio.TM.OnceCallback(function () sneedio._SneedioCampaignMain(); end, sneedio.SYSTEM_TICK * 5, "campaign once");
 	end
