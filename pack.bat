c35c90fd0cbd415cbbdecacedae04ea52e93e11b
admiralnelson <admiralofinternetmeme@outlook.com>
2022-03-29 07:15:35 +0800

    disabled dll auto gen for packing script

--- pack.bat
+++ pack.bat
@@ -2,22 +2,9 @@
 
 SET RPFM_CLI_EXE=F:\rpfm-2.0.2-windows\rpfm_cli.exe
 
-echo compiling libsneedio.dll into lua...
-powershell .\pack_main_dll_into_base64_lua.ps1 > script\_lib\_libsneedio_compiled_main_dll_.lua
-
-echo compiling dlls into lua...
-powershell .\pack_dlls_into_base64_lua.ps1 > script\_lib\_libsneedio_compiled_dlls_.lua
-
-echo compiling ytdlp into lua...
-powershell .\pack_ytdlp_into_base64_lua.ps1 > script\_lib\_libsneedio_compiled_ytdlp_.lua
-
 mkdir out
 %RPFM_CLI_EXE% --game warhammer_2 --packfile "%CD%\out\libsneedio.pack"  packfile --new-packfile
 %RPFM_CLI_EXE% --game warhammer_2 --packfile "%CD%\out\libsneedio.pack"  packfile --add-folders "script" "%CD%\script\_lib" "%CD%\script\battle" "%CD%\script\campaign" "%CD%\script\frontend" "%CD%\script\mct" 
-rem %RPFM_CLI_EXE% --game warhammer_2 --packfile "%CD%\out\libsneedio.pack"  packfile --add-folders "script" 
-rem %RPFM_CLI_EXE% --game warhammer_2 --packfile "%CD%\out\libsneedio.pack"  packfile --add-folders "script" 
-rem %RPFM_CLI_EXE% --game warhammer_2 --packfile "%CD%\out\libsneedio.pack"  packfile --add-folders "script" 
-rem %RPFM_CLI_EXE% --game warhammer_2 --packfile "%CD%\out\libsneedio.pack"  packfile --add-folders "script" 
 if not exist "%CD%\out\libsneedio.pack" (
 	echo if it's errored out, pos probably broken. pack it up  yourself
 )
